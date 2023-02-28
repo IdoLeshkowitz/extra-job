ec36dd7f0e326834127768c21ca262724429dabb
IdoLeshkowitz <idoleshkowitz@gmail.com>
2023-02-28 20:30:27 +0200

    landing page and next auth integrated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,27 @@
-'use client'
 import '../../styles/theme.scss'
-import ScrollTopButton from '../components/ScrollTopButton'
-import SSRProvider from 'react-bootstrap/esm/SSRProvider'
 import JobBoardPageLayout from "@/partials/JobBoardPageLayout";
+import Provider from './provider';
+import {getServerSession} from "next-auth";
+import {authOptions} from "@/pages/api/auth/[...nextauth]";
 
-
-export default function RootLayout({
-                                       children,
-                                   }: {
+export default async function RootLayout({
+                                             children,
+                                         }: {
     children: React.ReactNode
 }) {
     return (
-        <html lang="he" dir="rtl">
+        <html>
             {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-            <SSRProvider>
-                <body>
-                    <JobBoardPageLayout>
+            <body>
+                <Provider>
+                    <JobBoardPageLayout session={await getServerSession(authOptions)}>
                         {children}
                     </JobBoardPageLayout>
-                </body>
-
-                <ScrollTopButton
-                    showOffset={600}
-                    duration={800}
-                    easing='easeInOutQuart'
-                    tooltip='Top'
-                />
-            </SSRProvider>
-
+                </Provider>
+            </body>
         </html>
     )
 }
--- src/components/partials/JobBoardPageLayout.tsx
+++ src/components/partials/JobBoardPageLayout.tsx
@@ -1,35 +1,29 @@
 'use client';
 import {useState} from 'react'
 import SignInModalLight from '@/partials/SignInModalLight'
-import {Card, Col, Container, Row, SSRProvider} from "react-bootstrap";
+import {SSRProvider} from "react-bootstrap";
 import NavBar from "@/partials/NavBar";
-import ImageLoader from "@/components/ImageLoader";
-import Link from "next/link";
-import MarketButton from '@/components/MarketButton'
-import SocialButton from '@/components/SocialButton'
-import AddAreaModal from "@/partials/AddAreaModal";
-const JobBoardPageLayout = (props: any) => {
+import {Session} from "next-auth";
+import Footer from "@/components/Footer";
 
-    // Sign in modal
-    const [signinShow, setSigninShow] = useState(true)
-    const handleSigninClose = () => setSigninShow(false)
-    const handleSigninShow = () => setSigninShow(true)
+enum ModalState {
+    SignIn,
+    SignUp,
+    InActive
+}
 
-    // Sign up modal
-    const [signupShow, setSignupShow] = useState(false)
-    const handleSignupClose = () => setSignupShow(false)
-    const userLoggedIn = false
+interface JobBoardPageLayoutProps {
+    children: React.ReactNode
+    session: Session | null
+}
 
-    // Swap modals
+const JobBoardPageLayout = ({children, session}: JobBoardPageLayoutProps) => {
+    const [modalState, setModalState] = useState<ModalState>(ModalState.InActive)
     const handleSignInToUp = (e: any) => {
         e.preventDefault()
-        setSigninShow(false)
-        setSignupShow(true)
     }
     const handleSignUpToIn = (e: any) => {
         e.preventDefault()
-        setSigninShow(true)
-        setSignupShow(false)
     }
 
 
@@ -37,123 +31,21 @@ const JobBoardPageLayout = (props: any) => {
         <SSRProvider>
             <div>
                 {/* Sign in modal */}
-                {userLoggedIn && <SignInModalLight
+                {modalState ===  ModalState.SignIn && <SignInModalLight
                     centered
                     size='lg'
                     pillButtons
-                    show={signinShow}
-                    onHide={handleSigninClose}
-                    onSwap={handleSignInToUp}
+                    show={true}
+                    onHide={() => setModalState(ModalState.InActive)}
+                    onSwap={() => setModalState(ModalState.SignUp)}
                 />}
 
-                {/* Sign up modal */}
-                {userLoggedIn && <SignInModalLight
-                    centered
-                    size='lg'
-                    pillButtons
-                    show={signupShow}
-                    onHide={handleSignupClose}
-                    onSwap={handleSignUpToIn}
-                />}
-                navbar
-                {/* Page wrapper for sticky footer
-                Wraps everything except footer to push footer to the bottom of the page if there is little content */}
-
-                    {/* Navbar (main site header with branding and navigation) */}
-
-
-                    {/* Page content */}
-                    {props.children}
-                <NavBar userLoggedIn={true}/>
-
+                {/*PAGE CONTENT*/}
+                {children}
+                <NavBar session = {session} openSignInModal={()=>setModalState(ModalState.SignIn)}/>
 
                 {/* Footer */}
-                <footer className='footer bg-dark pt-5'>
-                    <Container className='pb-2'>
-                        <Row className='align-items-center pb-4'>
-                            <Col md={6} xl={5}>
-
-                                {/* Links */}
-                                <Row xs={1} sm={3} className='gy-4'>
-                                    <Col>
-                                        <h3 className='h6 mb-2 pb-1 fs-base text-light'>Finder</h3>
-                                        <ul className='list-unstyled fs-sm'>
-                                            <li><Link href='src/components/partials#' className='nav-link-light'>About
-                                                us</Link></li>
-                                            <li><Link href='src/components/partials#' className='nav-link-light'>News</Link>
-                                            </li>
-                                            <li><Link href='src/components/partials#'
-                                                      className='nav-link-light'>Contacts</Link></li>
-                                        </ul>
-                                    </Col>
-                                    <Col>
-                                        <h3 className='h6 mb-2 pb-1 fs-base text-light'>For Job Seekers</h3>
-                                        <ul className='list-unstyled fs-sm'>
-                                            <li><Link href='src/components/partials#' className='nav-link-light'>Find
-                                                job</Link></li>
-                                            <li><Link href='src/components/partials#' className='nav-link-light'>Post a
-                                                resume</Link></li>
-                                            <li><Link href='src/components/partials#' className='nav-link-light'>Vacancy
-                                                mailing</Link></li>
-                                        </ul>
-                                    </Col>
-                                    <Col>
-                                        <h3 className='h6 mb-2 pb-1 fs-base text-light'>For Employers</h3>
-                                        <ul className='list-unstyled fs-sm'>
-                                            <li><Link href='src/components/partials#' className='nav-link-light'>Find
-                                                resume</Link></li>
-                                            <li><Link href='src/components/partials#' className='nav-link-light'>Post a
-                                                job</Link></li>
-                                            <li><Link href='src/components/partials#' className='nav-link-light'>Resume
-                                                mailing</Link></li>
-                                        </ul>
-                                    </Col>
-                                </Row>
-
-                                {/* Socials */}
-                                <div className='text-nowrap border-top border-light mt-3 py-4'>
-                                    <SocialButton size="sm" href='#' brand='facebook' variant='translucent' roundedCircle light
-                                                  className='me-2'/>
-                                    <SocialButton size="sm" href='#' brand='twitter' variant='translucent' roundedCircle light
-                                                  className='me-2'/>
-                                    <SocialButton size="sm" href='#' brand='messenger' variant='translucent' roundedCircle light
-                                                  className='me-2'/>
-                                </div>
-                            </Col>
-                            <Col md={6} xl={{offset: 1}}>
-                                <div className='d-flex align-items-center'>
-                                    <Card className='card-light w-100' style={{maxWidth: '526px'}}>
-                                        <Card.Body className='p-4 p-xl-5 my-2 my-md-0'>
-                                            <div style={{maxWidth: '380px'}}>
-                                                <h3 className='h4 text-light'>Download Our App</h3>
-                                                <p className='fs-sm text-light opacity-70 mb-2 mb-lg-3'>Now finding the new
-                                                    job just got even easier with our new app!</p>
-                                                <div className='d-flex flex-column flex-sm-row ms-n3'>
-                                                    <MarketButton href='#' market='apple' target='_blank'
-                                                                  className='mt-3 ms-3'/>
-                                                    <MarketButton href='#' market='google' target='_blank'
-                                                                  className='mt-3 ms-3'/>
-                                                </div>
-                                            </div>
-                                        </Card.Body>
-                                    </Card>
-                                    <div className='d-none d-xl-block ms-n4'>
-                                        <ImageLoader priority src='/images/job-board/footer-mobile.svg' width={116}
-                                                     height={233} alt='Mobile app'/>
-                                    </div>
-                                </div>
-                            </Col>
-                        </Row>
-
-
-                        {/* Copyright */}
-                        <p className='fs-sm text-center text-sm-start mb-4'>
-                            <span className='text-light opacity-50'>&copy; All rights reserved. Made by </span>
-                            <a href='https://createx.studio/' className='nav-link-light fw-bold' target='_blank'
-                               rel='noreferrer'>Createx Studio</a>
-                        </p>
-                    </Container>
-                </footer>
+                <Footer/>
             </div>
         </SSRProvider>
     )
