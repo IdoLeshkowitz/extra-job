12303362fb8ad98afad55b053d0275e9b127b4a6
IdoLeshkowitz <idoleshkowitz@gmail.com>
2023-03-08 17:15:58 +0200

    admin-layout-modified

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,6 +3,7 @@ import GoogleProvider from "next-auth/providers/google";
 import {PrismaAdapter} from "@next-auth/prisma-adapter";
 import prisma from "../../../../lib/prisma";
 import Auth0Provider from "next-auth/providers/auth0";
+
 export const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(prisma),
     providers: [
@@ -15,7 +16,13 @@ export const authOptions: NextAuthOptions = {
             clientSecret: process.env.AUTH0_CLIENT_SECRET as any,
             issuer: process.env.AUTH0_ISSUER as any,
         }),
-    ]
+    ],
+    callbacks: {
+        async session({session, user}) {
+            session.user.role = user.role;
+            return session
+        }
+    }
 }
 
 export default NextAuth(authOptions);
