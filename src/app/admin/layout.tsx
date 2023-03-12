3a736d8dd397c365850ad1fa1fc19c6638bbf219
IdoLeshkowitz <idoleshkowitz@gmail.com>
2023-03-12 20:09:43 +0200

    added admin protection

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,6 +1,14 @@
 import AdminPageSideBar from "@/app/admin/components/AdminPageSideBar";
+import {getServerSession} from "next-auth";
+import {authOptions} from "@/pages/api/auth/[...nextauth]";
+import {Role} from "@prisma/client";
+import { redirect } from "next/navigation";
 
-export default function AdminLayout({children}: { children: React.ReactNode }) {
+export default async function AdminLayout({children}: { children: React.ReactNode }) {
+    const {user} = await getServerSession(authOptions) ??{}
+    if (!user || user.role !== Role.ADMIN){
+        return redirect('api/auth/signin')
+    }
     return (
         <div className='container pt-5 pb-lg-4 mt-5 mb-sm-2'>
             <div className="row">
