b20e211aa5ff313a3877588f03a8bc2c254587de
Shahar <shaharmes33@gmail.com>
2023-03-13 20:45:28 +0200

    bootstrap searchbar version

--- src/components/jobSearchBar/SearchJobBar.tsx
+++ src/components/jobSearchBar/SearchJobBar.tsx
@@ -7,23 +7,61 @@ import Button from "react-bootstrap/Button";
 import {object, string} from "yup";
 import {Form} from "react-bootstrap";
 import {useFormik} from "formik";
+import { Area, Profession } from "@prisma/client";
+import { useState, useEffect } from "react";
 
 const validationSchema = object({
-    text : string()
+    text : string(),
+    areaId: string(),
+    professionId: string()
 
 })
 export default function SearchJobBar() {
     const formik = useFormik({
         initialValues: {
             text: '',
+            areaId: '',
+            professionId: ''
         },
         validationSchema,
         onSubmit: (values) => {
             console.log(values)
         }
     })
+
+    const getAreas = async (): Promise<{ data: { areas: Area[] } }> => {
+        const res = await fetch('/api/area?active=true')
+        if (!res.ok) {
+            return Promise.reject(await res.json())
+        }
+        return await res.json()
+    }
+
+    const getProfessions = async (): Promise<{ data: { professions: Profession[] } }> => {
+        const res = await fetch('/api/profession')
+        if (!res.ok) {
+            return Promise.reject(await res.json())
+        }
+        return await res.json()
+    }
+
+    const [areas, setAreas] = useState<Area[]>([])
+    const [professions, setProfessions] = useState<Profession[]>([])
+    useEffect(() => {
+        Promise.all([getAreas(), getProfessions()])
+            .then(([{data: {areas}}, {data: {professions}}]) => {
+                setAreas(areas)
+                setProfessions(professions)
+            })
+            .catch((err) => {
+                console.error(err)
+            })
+    }, [])
+
+
     return (
-        <Form className='form-group d-block d-md-flex rounded-md-pill mb-2 mb-sm-4'>
+        <Form className='form-group d-block d-md-flex rounded-md-pill mb-2 mb-sm-4'
+        onSubmit={formik.handleSubmit}>
 
             {/*TEXT INPUT*/}
             <InputGroup size='lg' className='border-end-md' >
@@ -45,20 +83,17 @@ export default function SearchJobBar() {
             {/*AREA SELECT*/}
             <div className='d-sm-flex'>
                 <DropdownSelect
-                    name="name"
+                    name="areaId"
+                    value={formik.values.areaId}
                     darkMenu={true}
-                    defaultValue='All categories'
+                    defaultValue='בחר איזור'
                     icon='fi-list'
-                    options={[
-                        ['fi-bed', 'Accomodation'],
-                        ['fi-cafe', 'Food & Drink'],
-                        ['fi-shopping-bag', 'Shopping'],
-                        ['fi-museum', 'Art & Hisory'],
-                        ['fi-entertainment', 'Entertainment'],
-                        ['fi-meds', 'Medicine'],
-                        ['fi-makeup', 'Beauty'],
-                        ['fi-car', 'Car Rental']
-                    ]}
+                    options={areas.map((area) => ['fi-map-pin', area.name])}
+                    onBlur = {formik.handleBlur}
+                    onChange = {(selected: any) => {
+                        formik.setFieldValue('areaId', selected)
+                    }}
+                    selected={formik.values.areaId}
                     variant='link btn-lg ps-2 ps-sm-3'
                     className='w-100 mb-sm-0 mb-3'
                 />
@@ -67,22 +102,17 @@ export default function SearchJobBar() {
                 {/*CATEGORY SELECT*/}
                 <DropdownSelect
                     darkMenu={true}
-                    defaultValue='All categories'
+                    defaultValue='בחר מקצוע'
                     icon='fi-list'
-                    options={[
-                        ['fi-bed', 'Accomodation'],
-                        ['fi-cafe', 'Food & Drink'],
-                        ['fi-shopping-bag', 'Shopping'],
-                        ['fi-museum', 'Art & Hisory'],
-                        ['fi-entertainment', 'Entertainment'],
-                        ['fi-meds', 'Medicine'],
-                        ['fi-makeup', 'Beauty'],
-                        ['fi-car', 'Car Rental']
-                    ]}
+                    name="professionId"
+                    value={formik.values.professionId}
+                    onBlur = {formik.handleBlur}
+                    onChange = {formik.handleChange}
+                    options={professions.map((profession) => ['fi-geo', profession.name])}
                     variant='link btn-lg ps-2 ps-sm-3'
                     className='w-100 mb-sm-0 mb-3'
                 />
-                <Button size='lg' className='rounded-pill w-100 w-md-auto ms-sm-3'>Search</Button>
+                <Button size='lg' className='rounded-pill w-100 w-md-auto ms-sm-3' type="submit" >Search</Button>
             </div>
         </Form>
     )
