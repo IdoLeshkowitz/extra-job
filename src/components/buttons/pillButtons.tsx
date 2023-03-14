8f5f1b12435fddac10975f7ab1a01074a4ef0cc8
IdoLeshkowitz <idoleshkowitz@gmail.com>
2023-03-15 00:05:45 +0200

    fixed pillbutton

--- src/components/buttons/pillButtons.tsx
+++ src/components/buttons/pillButtons.tsx
@@ -16,7 +16,7 @@ const PillButton: FC<PillButtonProps> = ({text, type, onClick, icon}) => {
             type={type ? type : 'button'}
             className="icon-box card card-light flex-row align-items-center card-hover rounded-pill py-2 ps-2 pe-4"
         >
-            <div className="icon-box-media bg-faded-light text-light rounded-circle me-2 text-end">
+            <div className="icon-box-media bg-faded-light text-light rounded-circle me-2">
                 <i className={`${icon && icon} text-end`}/>
             </div>
             <h3 className="icon-box-title fs-sm text-light ps-1 mb-0">{text || ''}</h3>
