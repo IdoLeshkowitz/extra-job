9fdb1fc5d2464e44aa1f8611d206fab06a675edd
IdoLeshkowitz <idoleshkowitz@gmail.com>
2023-03-17 00:10:10 +0200

    attemp with formidable

--- src/pages/api/cv/index.ts
+++ src/pages/api/cv/index.ts
@@ -1,12 +1,10 @@
-//@ts-ignore
-import {createRouter} from "next-connect";
+import formidable from 'formidable'
 import {NextApiRequest, NextApiResponse} from "next";
-import multer from "multer";
+import fs from "fs";
 import {Prisma} from ".prisma/client";
 import {getServerSession} from "next-auth";
 import {authOptions} from "@/pages/api/auth/[...nextauth]";
 import prisma from "@/lib/prisma";
-import * as fs from "fs";
 import CvCreateInput = Prisma.CvCreateInput;
 
 export const config = {
@@ -14,50 +12,44 @@ export const config = {
         bodyParser: false
     }
 }
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/')
-    },
-    filename(req, file, callback) {
-        callback(null, file.originalname)
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'POST') {
+        const {user} = await getServerSession(req, res, authOptions) ?? {}
+        if (!user) {
+            res.status(401).end()
+            return
+        }
+        const form = new formidable.IncomingForm()
+        form.parse(req, async (err, fields, files) => {
+                const file = files.cv as formidable.File
+                const buffer = fs.readFileSync(file.filepath)
+                const cvCreateInput: CvCreateInput = {
+                    file: buffer,
+                    User: {connect: {id: user.id}},
+                }
+                try {
+                    const cv = await prisma.cv.create({data: cvCreateInput})
+                    res.status(200).json({cvCreateInput})
+                } catch (e) {
+                    console.log(e)
+                    res.status(500).json({message: "Internal server error"})
+                }
+            }
+        )
     }
-})
-const upload = multer({storage: storage})
-const router = createRouter<NextApiRequest & {file : any}, NextApiResponse>()
-// @ts-ignore
-router.use(upload.single("cv"))
-router.post(async (req, res) => {
-    const {user} = await getServerSession(req, res, authOptions) ?? {}
-    if (!user) {
-        res.status(401).json({message: "Unauthorized"})
-        return
+    if (req.method === 'GET') {
+        const {user} = await getServerSession(req, res, authOptions) ?? {}
+        if (!user) {
+            res.status(401).json({message: "Unauthorized"})
+            return
+        }
+        const [cv] = await prisma.cv.findMany({where: {userId: user.id}, orderBy: {createdAt: "desc"}, take: 1})
+        if (!cv) {
+            res.status(404).json({message: "Not found"})
+            return
+        }
+        res.setHeader("Content-Type", "application/pdf")
+        res.setHeader("Content-Disposition", "attachment; filename=cv.pdf")
+        res.send(cv.file)
     }
-    const file = fs.readFileSync(req.file.path)
-    const cvToCreate: CvCreateInput = {
-        file: file,
-        User: {connect: {id: user.id}}
-    }
-    try {
-        const createdCv = await prisma.cv.create({data: cvToCreate})
-        res.status(200).json({message: "OK"})
-    } catch (e) {
-        console.log(e)
-        res.status(500).json({message: "Internal server error"})
-    }
-})
-router.get(async (req, res) => {
-    const {user} = await getServerSession(req, res, authOptions) ?? {}
-    if (!user) {
-        res.status(401).json({message: "Unauthorized"})
-        return
-    }
-    const [cv] = await prisma.cv.findMany({where: {userId: user.id}, orderBy: {createdAt: "desc"}, take: 1})
-    if (!cv) {
-        res.status(404).json({message: "Not found"})
-        return
-    }
-    res.setHeader("Content-Type", "application/pdf")
-    res.setHeader("Content-Disposition", "attachment; filename=cv.pdf")
-    res.send(cv.file)
-})
-export default router.handler({})
+}
