165280baae051ecb190a405bb4ab9f6f83826d7c
Shahar <shaharmes33@gmail.com>
2023-03-23 23:09:12 +0200

    start building from to pdf system

---
+++ src/app/jobseeker/resume/page.tsx
@@ -0,0 +1,140 @@
+'use client';
+
+import { useRef, useState } from 'react'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import JobBoardPostResumeLayout from '../components/ResumeLayout'
+  
+import { Document, Page, Text, View, StyleSheet, pdf, PDFDownloadLink } from '@react-pdf/renderer';
+
+const styles = StyleSheet.create({
+    page: {
+      flexDirection: 'column',
+      backgroundColor: '#fff',
+      padding: 20,
+      fontFamily: 'Helvetica',
+    },
+    section: {
+      margin: 10,
+      padding: 10,
+      flexGrow: 1,
+    },
+    heading: {
+      fontSize: 18,
+      fontWeight: 'bold',
+      marginBottom: 10,
+    },
+    label: {
+      fontSize: 14,
+      fontWeight: 'bold',
+      marginBottom: 5,
+    },
+    value: {
+      fontSize: 14,
+      marginBottom: 10,
+    },
+});
+
+const PostResumePDF = () => (
+  <Document>
+    <Page size='A4' style={styles.page}>
+      <View>
+        <Text> Hello Leshko </Text>
+      </View>
+    </Page>
+  </Document>
+);
+
+const Resume = () => {
+
+    const [blob, setBlob] = useState<any>(false);
+    const [formData, setFormData] = useState<any>(null);
+   
+    const handleSubmit = (event: any) => {
+      event.preventDefault();
+      const data = {
+        firstName: event.target.elements['pr-fn'].value,
+        lastName: event.target.elements['pr-sn'].value,
+        email: event.target.elements['pr-email'].value,
+        phone: event.target.elements['pr-phone'].value,
+      };
+
+      console.log(data)
+      setFormData(data);
+      setBlob(true);
+    };
+
+    // const generatePdf = async () => {
+    //     const blobe = await pdf(<PostResumePDF />).toBlob();
+    //     setBlob(blobe);
+    // };
+
+    
+    return (
+      <JobBoardPostResumeLayout activeStep='Basic info'nextStep='/job-board/post-resume-2'>
+        <h2 className='h4 mb-4'>
+          <i className='fi-info-circle text-primary fs-5 mt-n1 me-2'></i>
+          Basic info
+        </h2>
+        <Form onSubmit={handleSubmit}>
+        <Row xs={1} sm={2} className='gy-4 mb-4'>
+          <Col as={Form.Group} controlId='pr-fn'>
+            <Form.Label>
+              First name
+              <span className='text-danger'>&nbsp;*</span>
+            </Form.Label>
+            <Form.Control size='lg' placeholder='Enter your first name' defaultValue='Annette' required/>
+          </Col>
+          <Col as={Form.Group} controlId='pr-sn'>
+            <Form.Label>
+              Second name
+              <span className='text-danger'>&nbsp;*</span>
+            </Form.Label>
+            <Form.Control size='lg' placeholder='Enter your second name' defaultValue='Black' required/>
+          </Col>
+          <Col as={Form.Group} controlId='pr-email'>
+            <Form.Label>
+              Email address
+              <span className='text-danger'>&nbsp;*</span>
+            </Form.Label>
+            <Form.Control size='lg' type='email' placeholder='Enter your email address' defaultValue='annette_black@email.com' required/>
+          </Col>
+          <Col as={Form.Group} controlId='pr-phone'>
+            <Form.Label>
+              Phone
+            </Form.Label>
+            <Form.Control  size='lg' type='tel' placeholder='Enter your phone number' defaultValue='(302) 555-0107' />
+          </Col>
+        </Row>
+        <div className='d-flex flex-column flex-sm-row bg-light rounded-3 p-4 px-md-5'>
+            <Button size='lg' variant='primary rounded-pill ms-auto' className='mt-sm-0 mt-3' type='submit'>Save resume</Button>
+        </div>
+        </Form>
+        <div className="d-flex flex-column flex-sm-row bg-light rounded-3 p-4 px-md-5">
+        {/* <Button
+          size="lg"
+          variant="primary rounded-pill ms-auto"
+          className="mt-sm-0 mt-3"
+          onClick={generatePdf}
+        >
+          Save and generate PDF
+        </Button> */}
+        {blob && (
+          <PDFDownloadLink
+            document={<PostResumePDF />}
+            fileName="somename.pdf"
+            className="btn btn-lg btn-outline-primary rounded-pill ms-3 mt-3 mt-sm-0"
+          >
+            {({ loading }) =>
+              loading ? 'Loading document...' : 'Download now!'
+            }
+          </PDFDownloadLink>
+        )}
+      </div>
+    </JobBoardPostResumeLayout>
+  )
+}
+
+export default Resume
